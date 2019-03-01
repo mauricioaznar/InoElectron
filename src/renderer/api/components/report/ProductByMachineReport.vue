5a248c776708624a19fe0c9c7a233134718bde4d
mauricioaznar <maurisio01>
2019-03-01 14:02:09 -0600

    v1.0.23

--- src/renderer/api/components/report/ProductByMachineReport.vue
+++ src/renderer/api/components/report/ProductByMachineReport.vue
@@ -93,23 +93,29 @@
             return
           }
           let statsRoute = ''
+          let wasteRoute = ''
           let machineTypeId = 0
           if (this.bagMode) {
             statsRoute = 'bagProductionByIntervalsReport'
+            wasteRoute = 'bagProductionWasteByIntervalsReport'
             machineTypeId = 1
           }
           if (this.rollMode) {
             statsRoute = 'rollProductionByIntervalsReport'
+            wasteRoute = 'rollProductionWasteByIntervalsReport'
             machineTypeId = 2
           }
           Promise.all([
             ApiOperations.getWithFilterExactWithoutPagination(EntityTypes.MACHINE, {machine_type_id: machineTypeId}),
             ApiOperations.getWithoutPagination(EntityTypes.MATERIAL),
-            ApiOperations.getStats(statsRoute)
+            ApiOperations.getStats(statsRoute),
+            ApiOperations.getStats(wasteRoute)
           ]).then(result => {
             let machines = result[0]
             let materials = result[1]
             let productionData = result[2]
+            let wasteData = result[3]
+            console.log(wasteData)
             this.bootstrapItems = []
             for (let year = 2018; year <= 2019; year++) {
               let yearItem = {}
@@ -132,29 +138,31 @@
               yearItem['monthItems'] = monthItems
               this.bootstrapItems.push(yearItem)
             }
-            this.setMachinesData(productionData, machines)
-            this.setMaterialsData(productionData, materials)
+            this.setMachinesData(productionData, wasteData, machines)
+            this.setMaterialsData(productionData, wasteData, materials)
             let vm = this
             this.timeout = setTimeout(function () {
               vm.isLoading = false
             }, 2000)
           })
         },
-        setMachinesData: function (machinesData, machines) {
+        setMachinesData: function (machinesData, wasteData, machines) {
           this.yearMachineTableFields = [{key: 'year', label: ''}, {key: 'show_details', label: ''}]
           this.monthMachineTableFields = [{key: 'month', label: 'mes'}]
             .concat(machines.map(machineObj => { return {key: 'machine' + machineObj.id, label: machineObj.name} }))
             .concat({key: 'total', label: 'total'})
+            .concat({key: 'waste', label: 'Desperdicio'})
             .concat({key: 'show_details', label: ''})
           this.dayMachineTableFields = [{key: 'day', label: 'dia'}, {key: 'weekday', label: 'dia de la semana'}]
             .concat(machines.map(machineObj => { return {key: 'machine' + machineObj.id, label: machineObj.name} }))
             .concat({key: 'total', label: 'total'})
+            .concat({key: 'waste', label: 'Desperdicio'})
           this.machineItems = cloneDeep(this.bootstrapItems)
           this.machineItems.forEach(yearMachineItem => {
             yearMachineItem['monthItems'].forEach(monthMachineItem => {
-              monthMachineItem = Object.assign(monthMachineItem, getTableRowDefaultMachineData(machines), {total: 0})
+              monthMachineItem = Object.assign(monthMachineItem, getTableRowDefaultMachineData(machines), {total: 0}, {waste: 0})
               monthMachineItem['dayItems'].forEach(dayMachineItem => {
-                dayMachineItem = Object.assign(dayMachineItem, getTableRowDefaultMachineData(machines), {total: 0})
+                dayMachineItem = Object.assign(dayMachineItem, getTableRowDefaultMachineData(machines), {total: 0}, {waste: 0})
               })
             })
           })
@@ -174,6 +182,19 @@
               dayItem['machine' + machineId] = Math.round((machineDataEfficiency + dayItem['machine' + machineId]) * 100) / 100
             }
           })
+          wasteData.forEach(wasteDataObj => {
+            let wasteDataMomentDate = moment(wasteDataObj.date_time, 'YYYY-MM-DD HH:mm:ss')
+            let wasteDataWaste = wasteDataObj.waste
+            let wasteDataYear = wasteDataMomentDate.year()
+            let wasteDataMonth = wasteDataMomentDate.month() + 1
+            let wasteDataDay = wasteDataMomentDate.date()
+            if (wasteDataYear >= 2018 && wasteDataYear <= 2019) {
+              let monthItem = this.machineItems[wasteDataYear - 2018].monthItems[wasteDataMonth - 1]
+              monthItem.waste = Math.round((wasteDataWaste + monthItem.waste) * 100) / 100
+              let dayItem = monthItem.dayItems[wasteDataDay - 1]
+              dayItem.waste = Math.round((wasteDataWaste + dayItem.waste) * 100) / 100
+            }
+          })
           function getTableRowDefaultMachineData (machines) {
             let tableRowDefaultData = {}
             machines.forEach(machineObj => {
@@ -182,21 +203,23 @@
             return tableRowDefaultData
           }
         },
-        setMaterialsData: function (materialsData, materials) {
+        setMaterialsData: function (materialsData, wasteData, materials) {
           this.yearMaterialTableFields = [{key: 'year', label: ''}, {key: 'show_details', label: ''}]
           this.monthMaterialTableFields = [{key: 'month', label: 'mes'}]
             .concat(materials.map(materialObj => { return {key: 'material' + materialObj.id, label: materialObj.name} }))
             .concat({key: 'total', label: 'total'})
+            .concat({key: 'waste', label: 'Desperdicio'})
             .concat({key: 'show_details', label: ''})
           this.dayMaterialTableFields = [{key: 'day', label: 'dia'}, {key: 'weekday', label: 'dia de la semana'}]
             .concat(materials.map(materialObj => { return {key: 'material' + materialObj.id, label: materialObj.name} }))
             .concat({key: 'total', label: 'total'})
+            .concat({key: 'waste', label: 'Desperdicio'})
           this.materialItems = cloneDeep(this.bootstrapItems)
           this.materialItems.forEach(yearMaterialItem => {
             yearMaterialItem['monthItems'].forEach(monthMaterialItem => {
-              monthMaterialItem = Object.assign(monthMaterialItem, getTableRowDefaultMaterialData(materials), {total: 0})
+              monthMaterialItem = Object.assign(monthMaterialItem, getTableRowDefaultMaterialData(materials), {total: 0}, {waste: 0})
               monthMaterialItem['dayItems'].forEach(dayMaterialItem => {
-                dayMaterialItem = Object.assign(dayMaterialItem, getTableRowDefaultMaterialData(materials), {total: 0})
+                dayMaterialItem = Object.assign(dayMaterialItem, getTableRowDefaultMaterialData(materials), {total: 0}, {waste: 0})
               })
             })
           })
@@ -214,6 +237,19 @@
               let dayItem = monthItem.dayItems[materialDataDay - 1]
               dayItem.total = Math.round((materialDataEfficiency + dayItem.total) * 100) / 100
               dayItem['material' + materialId] = Math.round((materialDataEfficiency + dayItem['material' + materialId]) * 100) / 100
+            }
+          })
+          wasteData.forEach(wasteDataObj => {
+            let wasteDataMomentDate = moment(wasteDataObj.date_time, 'YYYY-MM-DD HH:mm:ss')
+            let wasteDataWaste = wasteDataObj.waste
+            let wasteDataYear = wasteDataMomentDate.year()
+            let wasteDataMonth = wasteDataMomentDate.month() + 1
+            let wasteDataDay = wasteDataMomentDate.date()
+            if (wasteDataYear >= 2018 && wasteDataYear <= 2019) {
+              let monthItem = this.materialItems[wasteDataYear - 2018].monthItems[wasteDataMonth - 1]
+              monthItem.waste = Math.round((wasteDataWaste + monthItem.waste) * 100) / 100
+              let dayItem = monthItem.dayItems[wasteDataDay - 1]
+              dayItem.waste = Math.round((wasteDataWaste + dayItem.waste) * 100) / 100
             }
           })
           function getTableRowDefaultMaterialData (materials) {
