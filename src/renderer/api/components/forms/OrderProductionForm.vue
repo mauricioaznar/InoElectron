2c5b7ec50b1568b894ac123a833ae1de72e9c476
mauricioaznar <maurisio01>
2019-03-27 11:21:35 -0600

    v1.0.27

--- src/renderer/api/components/forms/OrderProductionForm.vue
+++ src/renderer/api/components/forms/OrderProductionForm.vue
@@ -18,7 +18,7 @@
                   :initialValue="initialValues[OrderProductionPropertiesReference.END_DATE_TIME.name]"
                   :error="errors.first(OrderProductionPropertiesReference.END_DATE_TIME.name)"
                   :disabled="!userHasWritePrivileges"
-                  v-validate="'required|date_format:yyyy-MM-DD HH:mm:ss|after:' + productionOrder.startDateTime"
+                  v-validate="'required|date_format:yyyy-MM-dd HH:mm:ss|after:' + productionOrder.startDateTime"
           >
           </mau-form-input-date-time>
           <div class="form-group" v-if="bagMode">
