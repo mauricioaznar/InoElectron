c5d580ea2475c84803780a0b642fb232b0932cf2
mauricioaznar <maurisio01>
2020-05-12 13:29:38 -0500

    desempenio

--- src/renderer/api/groups/home/components/ExpenseReportThree.vue
+++ src/renderer/api/groups/home/components/ExpenseReportThree.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <div class="danger">DANGER</div>
         <div class="row">
             <div class="col-sm-12 col-md-5">
                 <mau-form-input-date
