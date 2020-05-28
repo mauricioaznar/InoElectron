a0a2bb9b09c2248001cff1daa09c47e66d36d652
Maguas Tupaguas <mauricioaznar94@gmail.com>
2020-05-28 18:22:55 -0500

    machines to equipments report

--- src/renderer/api/components/inputs/MauTextInputSuggestedResults.vue
+++ src/renderer/api/components/inputs/MauTextInputSuggestedResults.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-        Sugerencias
-        <ul>
-            <li v-for="result in filteredResults">{{result[searchedProperty]}}</li>
-        </ul>
+    <div class="row">
+        <div class="col-sm-6">
+            Sugerencias ultima palabra
+            <ul>
+                <li v-for="result in filteredResultLastWord">{{result[searchedProperty]}}</li>
+            </ul>
+        </div>
+        <div class="col-sm-6">
+            Sugerencias toda las palabras
+            <ul>
+                <li v-for="result in filteredResultComplete">{{result[searchedProperty]}}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -13,7 +21,8 @@
       data () {
         return {
           test: 'This is a test for a text',
-          filteredResults: []
+          filteredResultLastWord: Array(10).fill(''),
+          filteredResultComplete: Array(10).fill('')
         }
       },
       props: {
@@ -34,18 +43,29 @@
         searchedValue: function (newSearchedValue) {
           let refactoredSearchedValues = newSearchedValue.split(' ')
           let refactoredSearchedValue = refactoredSearchedValues[refactoredSearchedValues.length - 1]
-          let filteredResults = []
+          let filteredResultLastWord = []
           GenericApiOperations.list(this.entityEndpointName, {filterLikes: {[this.searchedProperty]: refactoredSearchedValue}})
             .then(results => {
-              console.log(results)
               for (let i = 0; i < 10; i++) {
                 if (i < results.length) {
-                  filteredResults.push(results[i])
+                  filteredResultLastWord.push(results[i])
                 } else {
-                  filteredResults.push('')
+                  filteredResultLastWord.push('')
                 }
               }
-              this.filteredResults = filteredResults
+              this.filteredResultLastWord = filteredResultLastWord
+            })
+          let filteredResultComplete = []
+          GenericApiOperations.list(this.entityEndpointName, {filterLikes: {[this.searchedProperty]: newSearchedValue}})
+            .then(results => {
+              for (let i = 0; i < 10; i++) {
+                if (i < results.length) {
+                  filteredResultComplete.push(results[i])
+                } else {
+                  filteredResultComplete.push('')
+                }
+              }
+              this.filteredResultComplete = filteredResultComplete
             })
         }
       }
